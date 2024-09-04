b98d2d9d3878433076a3b104d503ff66cc44bd85
R4T10 <krit_tipnuan@hotmail.com>
2024-09-04 17:28:29 +0700

    add genre and path for get the newest movies

--- models/movie.js
+++ models/movie.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const movies = new mongoose.Schema({
     name: { type: String },
-    genre: {type:String},
+    genre: [{type:String}],
     description: { type: String },
     image: { type: String },
     rate: { type: Number }
-});
+},{ timestamps: true });
 module.exports = mongoose.model("movies",movies);
--- routes/movie.js
+++ routes/movie.js
@@ -3,30 +3,69 @@ var express = require('express');
 var router = express.Router();
 var movieModel = require('../models/movie')
 
-router.get("/api/v1/movie",async function(req,res,next){
-    try{
+
+router.get("/api/v1/movie", async function (req, res, next) {
+    try {
+        const movies = await movieModel.find()
+        return res.status(200).send({
+            data: movies
+        });
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({
+            message: "Fail to fetch movies",
+        })
+    }
+})
+
+router.get("/api/v1/movie_newest", async function (req, res, next) {
+    try {
+        const movies = await movieModel.aggregate([
+            {
+                $sort: { createdAt: -1 }
+            },
+            {
+                $limit: 5
+            }
+        ])
+        return res.status(200).send({
+            data: movies
+        });
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({
+            message: "Fail to fetch movies",
+        })
+    }
+})
+
+
+
+
+router.get("/api/v1/movie/select", async function (req, res, next) {
+    try {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 5;
         const movies = await movieModel.aggregate([
             {
-                $match:{}
+                $match: {}
             },
             {
-                $facet:{
-                    metaData:[
+                $facet: {
+                    metaData: [
                         {
-                            $count:'totalDocuments'
+                            $count: 'totalDocuments'
                         },
                         {
-                            $addFields:{
-                                pageNumber : page,
-                                totalPages: {$ceil :{$divide:["$totalDocuments",limit]}}
+                            $addFields: {
+                                pageNumber: page,
+                                totalPages: { $ceil: { $divide: ["$totalDocuments", limit] } }
                             }
                         }
                     ],
-                    data:[
+                    data: [
                         {
-                            $skip: (page-1)* limit
+                            $skip: (page - 1) * limit
                         },
                         {
                             $limit: limit
@@ -36,18 +75,18 @@ router.get("/api/v1/movie",async function(req,res,next){
             },
         ])
         return res.status(200).send({
-            data:movies
+            data: movies
         });
-    }catch(err){
+    } catch (err) {
         console.log(err)
         return res.status(500).send({
-            message:"Fail to fetch movies",
+            message: "Fail to fetch movies",
         })
     }
 })
 
-router.get("/api/v1/movie/:id",async function(req,res,next){
-    try{
+router.get("/api/v1/movie/:id", async function (req, res, next) {
+    try {
         let movieId = req.params.id;
         if (!mongoose.Types.ObjectId.isValid(id)) {
             return res.status(400).send({
@@ -59,41 +98,41 @@ router.get("/api/v1/movie/:id",async function(req,res,next){
         let movie = await movieModel.findById(movieId)
         return res.status(200).send({
             data: {
-                _id: movie.id, movie_name: movie.name,genre:movie.genre, image: movie.image, description: movie.description, rate: movie.rate
+                _id: movie.id, movie_name: movie.name, genre: movie.genre, image: movie.image, description: movie.description, rate: movie.rate
             }
         })
-    }catch(error){
+    } catch (error) {
         console.log(error)
         return res.status(500).send({
-            message:"Server error",
-            success:false,
+            message: "Server error",
+            success: false,
         })
     }
 })
 
 
 
-router.post("/api/v1/movie",async function(req,res,next){
-    try{
-        const {name,genre,description,image,rate} = req.body;
+router.post("/api/v1/movie", async function (req, res, next) {
+    try {
+        const { name, genre, description, image, rate } = req.body;
         let newMovie = new movieModel({
             name: name,
-            genre:genre,
-            description : description,
+            genre: genre,
+            description: description,
             image: image,
-            rate : rate
+            rate: rate
         });
         let movie = await newMovie.save();
         return res.status(201).send({
-            data:movie,
-            message:"Add new movie success",
-            success:true,
+            data: movie,
+            message: "Add new movie success",
+            success: true,
         });
-    }catch(error){
+    } catch (error) {
         console.log(error)
         return res.status(500).send({
-            message:"Fail to add new movie",
-            success:false,
+            message: "Fail to add new movie",
+            success: false,
         })
     }
 })
